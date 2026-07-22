99c3d4c5a45a2696f110b8196906e589b2889743
Kavee234 <kaveeshap81@gmail.com>
2026-07-22 14:32:06 +0530

    Update core-setup.c with edits

--- core-setup.c
+++ core-setup.c
@@ -111,3 +111,95 @@ void viewSeatMap(int m, int t)
     printf("------------------------------\n");
     printf("\n");
 }
+void bookSeat(int m, int t) {
+    char name[50];
+    char rowChar;
+    int col, discountChoice, groupSize;
+    int totalPrice = 0, totalDiscount = 0;
+    char bookedSeats[200] = "";
+
+    viewSeatMap(m, t);
+
+    printf("\nENTER YOUR FULL NAME (First and last): ");
+    scanf(" %[^\n]", name);
+
+    printf("BOOKING GROUP SIZE (Number of tickets): ");
+    scanf("%d", &groupSize);
+
+    for (int g = 0; g < groupSize; g++) {
+        viewSeatMap(m, t);
+
+        printf("\n");
+        printf("--------------------------------\n");
+        printf("== SELECT SEAT FOR TICKET %d  ==\n", g+1);
+        printf("--------------------------------\n");
+        printf("ENTER ROW (A-E): ");
+        scanf(" %c", &rowChar);
+        printf("ENTER SEAT NUMBER (1-10): ");
+        scanf("%d", &col);
+
+        int row = rowChar - 'A';
+        col--;
+
+        if (row < 0 || row >= ROWS || col < 0 || col >= COLS) {
+            printf("[X] INVALID SEAT SELECTION! [X]\n");
+            g--;
+            continue;
+        }
+        if (showtimes[m][t].seats[row][col].booked) {
+            printf("[X] SEAT ALREADY BOOKED! [X]\n");
+            g--;
+            continue;
+        }
+
+        int basePrice = getBasePrice(row);
+        int price = basePrice;
+        int seatDiscount = 0;
+
+        printf("\n");
+        printf("----------------------------------------------\n");
+        printf("==== SELECT DISCOUNT TYPE FOR THIS TICKET ====\n");
+        printf("----------------------------------------------\n");
+        printf("1. NONE\n");
+        printf("2. STUDENT (Age under 18 years)\n");
+        printf("3. SENIOR (18+)\n");
+        printf("----------------------------------------------\n");
+        printf("Choice: ");
+        scanf("%d", &discountChoice);
+
+        if (discountChoice == 2) { seatDiscount += basePrice * 0.1; }
+        else if (discountChoice == 3) { seatDiscount += basePrice * 0.2; }
+
+        if (groupSize >= 4) { seatDiscount += basePrice * 0.1; }
+
+        price = basePrice - seatDiscount;
+
+        showtimes[m][t].seats[row][col].booked = 1;
+        showtimes[m][t].seats[row][col].pricePaid = price;
+        strcpy(showtimes[m][t].seats[row][col].customerName, name);
+
+        totalPrice += price;
+        totalDiscount += seatDiscount;
+
+        char seatStr[10];
+        sprintf(seatStr, "%c%d ", 'A' + row, col+1);
+        strcat(bookedSeats, seatStr);
+    }
+
+    clearScreen();
+    printf("----------------------------------------------------\n");
+    printf("===========  [||] KDTIG MOVIE LAND [||]  ===========\n");
+    printf("----------------------------------------------------\n");
+    printf("   Movie           : %s\n", showtimes[m][t].movieName);
+    printf("   Showtime        : %s\n", showtimes[m][t].showtime);
+    printf("   Customer        : %s\n", name);
+    printf("   Number of Seats : %d\n", groupSize);
+    printf("   Seats Booked    : %s\n", bookedSeats);
+    printf("----------------------------------------------------\n");
+    printf("   Total Discount  : Rs. %d\n", totalDiscount);
+    printf("   Final Price Paid: Rs. %d\n", totalPrice);
+    printf("----------------------------------------------------\n");
+    printf("           THANK YOU FOR BOOKING WITH US!           \n");
+    printf("     VISIT OUR SITE : https://kdtigmovieland.com    \n");
+    printf("----------------------------------------------------\n");
+}
